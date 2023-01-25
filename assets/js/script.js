2548d57151bb18e4373df4c0d62d713d2dc041c4
Matthew <unicomatthew@gmail.com>
2023-01-25 15:53:05 -0800

    added animations to navBar, created the input for search city , created the containers for the information, and added google maps api and displayed it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
+//  document.addEventListener("ready", function(){
+//     document.querySelector('input#txtLocation').characterCounter();
+// });
+// hi;
+let map;
 
+function initMap() {
+  const localContextMapView = new google.maps.localContext.LocalContextMapView({
+    element: document.getElementById("map"),
+    placeTypePreferences: [
+      { type: "restaurant" },
+      { type: "tourist_attraction" },
+    ],
+    maxPlaceCount: 12,
+  });
+
+  map = localContextMapView.map;
+  map.setOptions({
+    center: { lat: 40.7128, lng: -74.0060 },
+    zoom: 12,
+  });
+}
+
+window.initMap = initMap;
